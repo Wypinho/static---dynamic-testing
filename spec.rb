fc9e0f01a8f8054c500d41e59a02290969f7ab05
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-03 15:21:56 +0000

    specs file created, writing tests

---
+++ spec.rb
@@ -0,0 +1,33 @@
+require('pry')
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# require_relative('./card_game')
+require_relative('./card')
+
+class TestCardGame < Minitest::Test
+
+  def setup
+    @card1 = Card.new("clubs", 3)
+    @card2 = Card.new("hearts", 13)
+    @card3 = Card.new("diamonds", 1)
+  end
+
+  def test_card_has_suit
+    assert_equal("clubs", @card1.suit)
+  end
+
+  def test_card_has_value
+    assert_equal(13, @card2.value)
+  end
+
+  def test_check_for_ace
+    assert_true(check_for_ace(@card3))
+  end
+
+  def test_highest_card
+    assert_equal(@card2, highest_card(@card1, @card2))
+  end
+
+end
